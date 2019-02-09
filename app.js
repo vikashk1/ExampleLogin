4d488f12ed7420988eefd6ee3efdea18ac8ae0ab
vikash <vikashk@grazitti.com>
2019-02-09 17:25:37 +0530

    Hroku Dependency added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const dbServices = require('./dbServices');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
